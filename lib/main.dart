dd3bcaeceaa7c175e68635d7170adb4ba661f963
thenoobtechie <inbox.deepakjoshi@gmail.com>
2019-05-31 21:36:20 +0530

    Magic 8 Ball challenge complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,42 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      MaterialApp(home: MainPage()),
+    );
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ask Me Anything"),
+        backgroundColor: Colors.blue[800],
+      ),
+      body: AskMeAnything(),
+      backgroundColor: Colors.blue,
+    );
+  }
+}
+
+class AskMeAnything extends StatefulWidget {
+  @override
+  _AskMeAnythingState createState() => _AskMeAnythingState();
+}
+
+class _AskMeAnythingState extends State<AskMeAnything> {
+  int imgNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset("images/ball$imgNumber.png"),
+        onPressed: () {
+          setState(() {
+            imgNumber = Random().nextInt(5) + 1;
+          });
+        },
       ),
     );
+  }
+}
